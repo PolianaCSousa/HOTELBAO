a697fcfc5eab2c1bf4c55386f45e44edf6355a68
Poliana <polianasamonte@gmail.com>
2025-05-25 15:43:25 -0300

    Criação do projeto

---
+++ src/test/java/com/hotelbao/HotelbaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelbao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelbaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
